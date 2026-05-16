5e58ba6658d4e013e4e229c831c4801a3bddec58
yelo <131483807+xxade2q@users.noreply.github.com>
2026-05-16 12:36:36 +0800

    Update server.js

--- server.js
+++ server.js
@@ -2,17 +2,19 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-let activeKeys = ["EXO-TEST-KEY-1234"];
-
 app.get('/', (req, res) => {
     res.send("Authentication Server is Online! 🚀");
 });
 
 app.get('/checkkey', (req, res) => {
     const userKey = req.query.key;
-    if (!userKey) return res.send("no_key_provided");
 
-    if (activeKeys.includes(userKey)) {
+    if (!userKey) {
+        return res.send("no_key_provided");
+    }
+
+    // MASTER RULE: If the key starts with "EXO-", automatically let them in!
+    if (userKey.startsWith("EXO-")) {
         res.send("valid");
     } else {
         res.send("invalid");
